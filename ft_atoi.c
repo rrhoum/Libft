5c4f2fe3fe4b68cf1efce9f74adbcc99bbaf3151
Rony RHOUM <rrhoum@e2r1p13.42.fr>
2014-11-17 17:39:16 +0100

    Libft

---
+++ ft_atoi.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rrhoum <rrhoum@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/17 16:27:17 by rrhoum            #+#    #+#             */
+/*   Updated: 2014/11/17 17:36:53 by rrhoum           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_atoi(const char *str)
+{
+	int i;
+	int neg;
+	int nbr;
+
+	i = 0;
+	neg = 1;
+	nbr = 0;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-')
+		neg = -1;
+	if ((str[i] == '-') || (str[i] == '+'))
+		i++;
+	while (str[i] != '\0' && ft_isdigit(str[i]))
+	{
+		nbr *= 10;
+		nbr += str[i] - 48;
+		i++;
+	}
+	return (nbr * neg);
+}
