46f148fd6e8a17551331378fd8f103cd6f33379c
rrhoum <rrhoum@e1r6p18.42.fr>
2014-11-20 17:10:52 +0100

    Libft

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: rrhoum <rrhoum@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 17:17:15 by rrhoum            #+#    #+#             */
-/*   Updated: 2014/11/19 19:08:57 by rrhoum           ###   ########.fr       */
+/*   Updated: 2014/11/20 16:59:56 by rrhoum           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void				*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t			i;
-	const unsigned char		*s1;
+	unsigned char	*s1;
 
 	i = 0;
 	s1 = (unsigned char *)s;
--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: rrhoum <rrhoum@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/18 16:20:52 by rrhoum            #+#    #+#             */
-/*   Updated: 2014/11/18 16:40:35 by rrhoum           ###   ########.fr       */
+/*   Updated: 2014/11/20 17:09:46 by rrhoum           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	if (ft_strncmp(s1, s2, n) == 0)
-		return (1);
-	else
-		return (0);
+	if (s1 && s2 && n)
+	{
+		if (ft_strncmp(s1, s2, n) == 0)
+			return (1);
+		else
+			return (0);
+	}
+	return (0);
 }
